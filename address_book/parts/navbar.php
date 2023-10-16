6281f85e2db99c55ae4a96d76036d6591112e299
weiyann <jona11105@gmail.com>
2023-10-16 14:24:53 +0800

    登入的表單

--- address_book/parts/navbar.php
+++ address_book/parts/navbar.php
@@ -29,8 +29,12 @@ if(!isset($pageName)){
             </li>
             <li class="nav-item">
               <a class="nav-link <?= $pageName=='cate2' ? 'active':'' ?>" href="cate2.php">二層選單</a>
+            </li>            
+          </ul>
+          <ul class="navbar-nav mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link <?= $pageName=='list' ? 'active':'' ?>"  href="login.php">登入</a>
             </li>
-            
             
           </ul>
           
