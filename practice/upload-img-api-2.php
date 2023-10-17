ec41ae984f03df9514f4188aba221786f768d3b9
weiyann <jona11105@gmail.com>
2023-10-17 10:42:33 +0800

    上傳檔案&上傳多個檔案api

---
+++ practice/upload-img-api-2.php
@@ -0,0 +1,47 @@
+<?php
+$dir = __DIR__ . '/../uploads/';
+# 檔案類型的篩選
+$exts = [
+  'image/jpeg' => '.jpg',
+  'image/png' => '.png',
+  'image/webp' => '.webp',
+];
+
+$output = [
+  'success' => false,
+  'files' => []
+];
+
+if (!empty($_FILES) and !empty($_FILES['photos'])) {
+
+  # 是不是陣列
+  if (is_array($_FILES['photos']['name'])) {
+
+    foreach ($_FILES['photos']['name'] as $i => $name) {
+
+      if (!empty($exts[$_FILES['photos']['type'][$i]])) {
+        $ext = $exts[$_FILES['photos']['type'][$i]]; // 副檔名
+
+        # 隨機的主檔名
+        $f = sha1($name . uniqid() . rand());
+
+        if (
+          move_uploaded_file(
+            $_FILES['photos']['tmp_name'][$i],
+            $dir . $f . $ext
+          )
+        ) {
+
+          $output['files'][] = $f . $ext;  // array push
+        }
+      }
+    }
+
+    if (count($output['files'])) {
+      $output['success'] = true;
+    }
+  }
+}
+
+header('Content-Type: application/json');
+echo json_encode($output);
